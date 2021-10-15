11769f5713da1a935108cb9b305d5b6f664a1cb4
Richard <richard.wigley@gmail.com>
2021-10-15 13:53:55 +0100

    Tip html 'form' added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import React, { useState } from 'react'
 import './index.css';
 import logo from './logo.svg'
 
 function App() {
+  const [bill, setBill] = useState(0)
+
+  const handleChangeBill = (e) => {
+    const value = e.target.value.replace(/\D/g, "");
+    if (value === '')
+      return;
+
+    console.log("bill:" + bill)
+    setBill(value);
+  };
+
   return (
-    <div className="App font-mono">
+    <div className="App font-mono bg-emperor-300">
       <header className="h-36 flex justify-center items-center">
         <img src={logo} className="h-12" alt="logo" />
       </header>
-    </div>
+      <section className="bg-white rounded-t-3xl border">
+        {/*border added to section to stop margin collapse with inner div*/}
+
+        <div className="m-8">
+          {/*form wrapper*/}
+
+        </div>
+     </section>
+   </div>
   );
 }
 
