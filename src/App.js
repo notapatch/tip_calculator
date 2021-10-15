8163af8c4f67c91d2060f20dea7a9c5e1bbe09c6
Richard <richard.wigley@gmail.com>
2021-10-15 11:17:07 +0100

    Logo svg added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './index.css';
+import logo from './logo.svg'
 
 function App() {
   return (
     <div className="App font-mono">
+      <header className="h-36 flex justify-center items-center">
+        <img src={logo} className="h-12" alt="logo" />
+      </header>
     </div>
   );
 }
