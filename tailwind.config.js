9a353cae84e4f39a4aabb591b8bda9427a0d6337
Richard <richard.wigley@gmail.com>
2021-10-25 10:12:37 +0100

    Tailwind disable purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ const colors = require('tailwindcss/colors')
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    enabled: false,
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  },
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
